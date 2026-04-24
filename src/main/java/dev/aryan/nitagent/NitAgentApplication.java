9c983ae7368e50dd783fb1daa12c17137f8efea4
Aryan <aryanachauhan21@gmail.com>
2026-04-23 21:40:57 -0700

    init: initial commit of project structure

---
+++ src/main/java/dev/aryan/nitagent/NitAgentApplication.java
@@ -0,0 +1,13 @@
+package dev.aryan.nitagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitAgentApplication.class, args);
+    }
+
+}
